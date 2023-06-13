1fee664d87cda6baa017778788be8efcc66d78bc
Mridul-Islam <1610947@iub.edu.bd>
2023-06-13 23:26:09 +0600

    Inertia install to Laravel and Vue

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Hi, This is {{ name }}.</h1>
+
+    <p>In this Series we will use the following frameworks.</p>
+
+    <ul>
+        <li v-for="framework of frameworks" :key="framework" v-text="framework"></li>
+    </ul>
+
+</template>
+
+
+<script>
+    export default {
+        props: {
+            name: String,
+            frameworks: Array
+        }
+    };
+</script>
